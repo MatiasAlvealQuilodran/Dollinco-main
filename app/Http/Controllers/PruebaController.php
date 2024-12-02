739c181456f3158e2128e6058cbccdcda262a3da
MatiasAlvealQuilodran <matias.alveal@virginiogomez.cl>
2024-12-02 02:13:29 -0300

    Descripción de los cambios realizados

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Http\Requests\AlumnoRequest;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
